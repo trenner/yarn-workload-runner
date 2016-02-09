000d93700b5cc5b37e303b46d357cc1881048d28
joh-mue <yesiamkeen@gmail.com>
2016-02-09 16:04:50 +0100

    jobParser works, some cleanup

--- src/main/java/Core/YarnTimedGenerator.java
+++ src/main/java/Core/YarnTimedGenerator.java
@@ -1,5 +1,8 @@
 package Core;
 
+import parser.JobParser;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class YarnTimedGenerator implements Runnable {
@@ -7,27 +10,22 @@ public class YarnTimedGenerator implements Runnable {
     public void run() {
         System.out.println("Thread running.");
 
-        // Load Jobs
-        ArrayList<Job> jobs = new ArrayList<Job>();
-        jobs.add(new FlinkJob());
+        // Load Jobs from jobfile TODO:un-hardcode this
+        File jobFile = new File("/Users/Johannes/arbeit/yarn-timed-workload-generator/src/main/resources/jobs.xml");
+        ArrayList<Job> jobs = JobParser.parseJobs(jobFile);
 
         // Load Config(s)
 
         // Load Schedule
-        ArrayList<Long> delays = new ArrayList<Long>();
-        // add delays
+        ArrayList<Long> delays = new ArrayList<>();
 
         int i = 0;
         for (Job job : jobs) {
             job.setDelay(delays.get(i++));
         }
 
-        YarnTimedGenerator runner = new YarnTimedGenerator();
-        runner.run();
-
-        // collect logs
-
         // run jobs according to schedule
+        // TODO: this needs to happen in different threads
         Yarn yarn = new Yarn();
         for (Job job: jobs) {
             try {
@@ -35,10 +33,13 @@ public class YarnTimedGenerator implements Runnable {
             } catch (Exception e) {e.printStackTrace();}
             yarn.submitApplication(job);
         }
+
+        // collect logs
     }
 
     public static void main(String[] args) {
         System.out.println("Running.");
+        // TODO: appropriate threading
         (new Thread(new YarnTimedGenerator())).start();
     }
 }
--- src/main/java/parser/JobParser.java
+++ src/main/java/parser/JobParser.java
@@ -1,11 +1,9 @@
 package parser;
 
-import org.w3c.dom.Document;
+import Core.Job;
+import org.w3c.dom.*;
 
 import org.apache.log4j.Logger;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -18,51 +16,56 @@ import Core.FlinkJob;
  * Created by Johannes on 03/02/16.
  */
 public class JobParser {
-
-    public static void main(String[] args) {
-        parseJobs(new File("/Users/Johannes/arbeit/yarn-timed-workload-generator/src/main/resources/jobs.xml"));
-    }
-
     private static final Logger LOG = Logger.getLogger(JobParser.class);
 
-    public static void parseJobs(File jobFile) {
+    public static ArrayList<Job> parseJobs(File jobFile) {
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = dbFactory.newDocumentBuilder();
             Document doc = documentBuilder.parse(jobFile);
-
             doc.getDocumentElement().normalize();
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
 
-            NodeList nodeList = doc.getElementsByTagName("job");
+            NodeList jobNodeList = doc.getElementsByTagName("job");
 
-            ArrayList<Element> jobs = new ArrayList<Element>();
+            ArrayList<Element> jobs = new ArrayList<>();
 
-            for (int i = 0; i < nodeList.getLength(); i++) {
-                Node node = nodeList.item(i);
+            for (int i = 0; i < jobNodeList.getLength(); i++) {
+                Node node = jobNodeList.item(i);
                 if (node.getNodeType() == Node.ELEMENT_NODE) {
                     jobs.add((Element) node);
                 }
             }
 
-            for (Element job : jobs) {
+            ArrayList<Job> jobList = new ArrayList<>();
+
+            // per Job
+            for (Element jobElement : jobs) {
+                // TODO: get framework and create appropriate job
                 FlinkJob flinkJob = new FlinkJob();
-                flinkJob.setJarFile(job.getElementsByTagName("jar").item(0).getTextContent());
+                // get jar path
+                flinkJob.setJarFile(jobElement.getElementsByTagName("jar").item(0).getTextContent());
                 // get parameters
-                NodeList parameters = job.getElementsByTagName("parameters");
+                // there is only 1 "parameters" node. it contains multiple parameter-elements
+                NodeList parameters = jobElement.getElementsByTagName("parameters").item(0).getChildNodes();
                 for (int i = 0; i < parameters.getLength(); i++) {
                     Node parameter = parameters.item(i);
                     if (parameter.getNodeType() == Node.ELEMENT_NODE) {
-                        String argument = parameter.getAttributes().;
-                        flinkJob.addArgument(argument, value);
+                        String argument = parameter.getAttributes().item(0).getNodeValue();
+                        // String argument = p.toString();
+                        String value = parameter.getTextContent();
+                        flinkJob.addParameter(argument, value);
                     }
                 }
-
-                flinkJob.setDelay(0);
+                System.out.println(flinkJob.getCommand());
+                jobList.add(flinkJob);
             }
 
+            System.out.println(jobList);
+            return jobList;
         } catch (Exception e) {
             e.printStackTrace();
         }
+        // TODO: refactor, this should never be reached, thus not necessary
+        return null;
     }
 }
