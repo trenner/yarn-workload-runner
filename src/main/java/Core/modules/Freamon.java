c5ca1c738ba8c11654246b6910e6a4179b3d262d
Gjum <code.gjum@gmail.com>
2016-04-07 17:11:28 +0200

    Implement Freamon module

--- src/main/java/Core/modules/Freamon.java
+++ src/main/java/Core/modules/Freamon.java
@@ -1,20 +1,49 @@
 package Core.modules;
 
-import Core.Job;
+import akka.actor.ActorRef;
+import akka.actor.UntypedActor;
+import util.Config;
 
 /**
- * Created by joh-mue on 07/04/16.
+ * Sends messages to the Freamon MonitorMasterActor
  */
-public class Freamon{
-    public static void onSubmit(String jobID) {
+public class Freamon {
 
+    private static final ActorRef freamonMaster = new UntypedActor() {
+        @Override
+        public void onReceive(Object message) throws Exception {
+            throw new RuntimeException("Received unexpected message " + message);
+        }
+    }.context().actorFor(Config.getInstance().getConfigItem("freamonMasterAddress"));
+
+    public static class OnStart {
+        public final String jobID;
+        public final long startTime;
+
+        public OnStart(String jobID, long startTime) {
+            this.jobID = jobID;
+            this.startTime = startTime;
+        }
+    }
+
+    public static class OnStop {
+        public final String jobID;
+        public final long stopTime;
+
+        public OnStop(String jobID, long stopTime) {
+            this.jobID = jobID;
+            this.stopTime = stopTime;
+        }
+    }
+
+    public static void onSubmit(String jobID) {
     }
 
     public static void onStart(String jobID, long startTime) {
-
+        freamonMaster.tell(new OnStart(jobID, startTime), null);
     }
 
-    public static void onStop(String jobID, long endTime) {
-
+    public static void onStop(String jobID, long stopTime) {
+        freamonMaster.tell(new OnStop(jobID, stopTime), null);
     }
 }
