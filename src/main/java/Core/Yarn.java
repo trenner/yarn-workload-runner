a8b19919a8bf97a41e0459efbb4d56a2b2a32e9b
Thomas Renner <thomas.renner@tu-berlin.de>
2016-06-15 22:06:08 +0200

    Add dstat integration

--- src/main/java/Core/Yarn.java
+++ src/main/java/Core/Yarn.java
@@ -6,17 +6,20 @@ import util.Config;
 import util.Schedule;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 
 /**
  * Created by joh-mue on 01/02/16.
- *
  */
 public class Yarn {
     private Schedule schedule;
     private PrintStream summaryLog;
+    //   private String dstatCmd = "dstat --epoch --cpu -C total --mem --net -N total --disk -D total --noheaders --nocolor --output ";
+    //   private String[] slaves = {"wally071", "wally072", "wally073", "wally074", "wally075", "wally076", "wally077", "wally078", "wally079", "wally080", "wally081", "wally082", "wally083", "wally084", "wally085", "wally086", "wally087", "wally088", "wally059", "wally090", "wally091", "wally092", "wally093", "wally094", "wally095", "wally096", "wally097", "wally098", "wally099", "wally100", "wally101", "wally060", "wally103", "wally104", "wally105", "wally106", "wally107", "wally108", "wally109", "wally110"};
+    private String[] slaves = Config.getInstance().getSlaves();
+    final private String dstatCmd = Config.getInstance().getDstatCmd();
 
     final static Logger LOG = Logger.getLogger(Yarn.class);
 
@@ -26,9 +29,33 @@ public class Yarn {
     }
 
     public void initiateJobExecution() {
-        for (JobSequence jobSequence: schedule) {
-            Thread jobThread = new Thread(new JobRunner(jobSequence));
-            jobThread.start();
+        if (Config.getInstance().runDstat()) {
+            try {
+                // start with dstat
+                Process[] dstatProcessArr = new Process[slaves.length];
+                Runtime.getRuntime().exec("mkdir -p" + Config.getInstance().getDstatCmd() + Config.getInstance().getConfigItem("log-dir") + "/" + schedule.getExperimentName() + "/dstats/", new String[0]);
+                for (int i = 0; i < slaves.length; i++) {
+                    String slave = slaves[i];
+                    LOG.info("start dstat with: ssh " + slave + " " + dstatCmd + Config.getInstance().getConfigItem("log-dir") + "/" + schedule.getExperimentName() + "/dstats/dstat-" + slave + ".csv");
+                    dstatProcessArr[i] = Runtime.getRuntime().exec("ssh " + slave + " " + dstatCmd + Config.getInstance().getConfigItem("log-dir") + "/" + schedule.getExperimentName() + "/dstats/dstat-" + slave + ".csv");
+                }
+
+                for (JobSequence jobSequence : schedule) {
+                    Thread jobThread = new Thread(new JobRunner(jobSequence));
+                    jobThread.start();
+                }
+                for (Process process1 : dstatProcessArr) {
+                    process1.destroy();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            //start without dstat
+            for (JobSequence jobSequence : schedule) {
+                Thread jobThread = new Thread(new JobRunner(jobSequence));
+                jobThread.start();
+            }
         }
     }
 
@@ -40,17 +67,20 @@ public class Yarn {
         public JobRunner(JobSequence jobSequence) {
             this.jobSequence = jobSequence;
             config = Config.getInstance();
-            envp = new String[] { "HADOOP_CONF_DIR=" + config.getHadoopConfDir() };
+            envp = new String[]{"HADOOP_CONF_DIR=" + config.getHadoopConfDir()};
         }
 
         public void run() {
-                for (Job job: jobSequence) {
-                    executeJob(job);
-                }
+            for (Job job : jobSequence) {
+                executeJob(job);
+            }
+
+
         }
 
         private void executeJob(Job job) {
             try {
+
                 LOG.info("Waiting " + job.getDelay() + "seconds to execute " + job.getJobName());
                 summaryLog.println("Waiting " + job.getDelay() + "seconds to execute " + job.getJobName());
                 Thread.sleep(job.getDelay() * 1000, 0);
@@ -92,14 +122,15 @@ public class Yarn {
                         long endTime = System.currentTimeMillis();
                         long duration = (endTime - startTime);
                         LOG.info("Took " + duration / 1000
-                            + " seconds to complete executing " + job + '+' + job.getDelay() + "sec");
-                        summaryLog.println("["+ Thread.currentThread().getName() + "]" +job.getJobName() + " - " + duration);
+                                + " seconds to complete executing " + job + '+' + job.getDelay() + "sec");
+                        summaryLog.println("[" + Thread.currentThread().getName() + "]" + job.getJobName() + " - " + duration);
                         if (config.notifyFreamon()) {
                             Freamon.onStop(job.getJobID(), endTime);
                         }
                         jobStarted = false;
                     }
                 }
+
 
                 // TODO: create experiment summary file/output
                 logPrintStream.flush();
